60cc4fc0b22ca36698173b4f71a83dfaa8fab977
Hulyaoren <orenhulyaa@gmail.com>
2023-11-05 11:35:19 -0500

    v2

--- src/main/java/practice1/Test.java
+++ src/main/java/practice1/Test.java
@@ -19,6 +19,9 @@ public class Test {
         //I am creating version 2
 
         System.out.println("Version 2");
+        int x =1;
+        int y= 2;
+        System.out.println(x+y);
  }
 //    static int numOfStudents =342;
 //public int myMethod(char c1, char c2){
